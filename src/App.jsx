8f7a43e11d43e440856828ee5a7af2a277bf103f
Fabrice <codinglone@gmail.com>
2023-05-02 11:45:51 +0200

    Added real faker

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,56 @@ import {
   Legend
 } from 'chart.js'
 import { Bar } from 'react-chartjs-2';
-import faker from 'faker'
+import { faker } from '@faker-js/faker'
 
 function App() {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  )
+
+   const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      }
+    }
+  }
+
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000})),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)'
+      }
+    ]
+  }
+
   return (
     <>
       <div>
-      <Doughnut />
+      <Bar options={options} data={data} />
       </div>
     </>
   );
 }
+
 
 export default App;
