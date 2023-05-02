f4736653dca02d3357a57fc807df9f563d88957b
Fabrice <codinglone@gmail.com>
2023-05-02 11:29:05 +0200

    Added imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { Doughnut } from 'react-chartjs-2';
-
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker'
 
 function App() {
   return (
