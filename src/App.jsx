521051531ddc0b81d9c041804490408ce070d35d
Fabrice <codinglone@gmail.com>
2023-05-02 11:49:13 +0200

    Finalized app

--- src/App.jsx
+++ src/App.jsx
@@ -40,19 +40,19 @@ function App() {
       {
         label: 'Dataset 1',
         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        backgroundColor: 'teal',
       },
       {
         label: 'Dataset 2',
         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000})),
-        backgroundColor: 'rgba(53, 162, 235, 0.5)'
+        backgroundColor: 'red'
       }
     ]
   }
 
   return (
     <>
-      <div>
+      <div style={{width: "45vw"}}>
       <Bar options={options} data={data} />
       </div>
     </>
